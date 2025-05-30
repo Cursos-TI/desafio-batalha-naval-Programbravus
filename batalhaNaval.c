476780494cb7acedb5856d564690a2a620570b30
Programbravus <allanmks21@gmail.com>
2025-05-30 22:47:11 +0000

    aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,30 +1,57 @@
+
 #include <stdio.h>
 
 #define TAMANHO_TABULEIRO 10
-#define TAMANHO_NAVIO_VERTICAL 3
-#define TAMANHO_NAVIO_HORIZONTAL 4
+#define NAVIO 3 // Representa um navio no tabuleiro
+#define VAZIO 0 // Representa espaço sem navio
+
+void inicializarTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = VAZIO;
+        }
+    }
+}
+
+void posicionarNavios(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    // Navio vertical na coluna 2
+    for (int i = 2; i < 5; i++) {
+        tabuleiro[i][2] = NAVIO;
+    }
+
+    // Navio horizontal na linha 6
+    for (int j = 4; j < 8; j++) {
+        tabuleiro[6][j] = NAVIO;
+    }
+
+    // Navio diagonal crescente (subindo à direita)
+    for (int i = 0; i < 4; i++) {
+        tabuleiro[5 - i][4 + i] = NAVIO;
+    }
+
+    // Navio diagonal decrescente (descendo à direita)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[7 + i][2 + i] = NAVIO;
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    printf("\nTabuleiro:\n");
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    // Definição das coordenadas dos navios
-    int navio_vertical[TAMANHO_NAVIO_VERTICAL][2] = {
-        {2, 3}, {3, 3}, {4, 3}  // Verticalmente na coluna 3
-    };
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
 
-    int navio_horizontal[TAMANHO_NAVIO_HORIZONTAL][2] = {
-        {6, 1}, {6, 2}, {6, 3}, {6, 4}  // Horizontalmente na linha 6
-    };
-
-    // Exibindo as coordenadas do navio vertical
-    printf("Coordenadas do Navio Vertical:\n");
-    for (int i = 0; i < TAMANHO_NAVIO_VERTICAL; i++) {
-        printf("(%d, %d)\n", navio_vertical[i][0], navio_vertical[i][1]);
-    }
-
-    // Exibindo as coordenadas do navio horizontal
-    printf("\nCoordenadas do Navio Horizontal:\n");
-    for (int i = 0; i < TAMANHO_NAVIO_HORIZONTAL; i++) {
-        printf("(%d, %d)\n", navio_horizontal[i][0], navio_horizontal[i][1]);
-    }
+    inicializarTabuleiro(tabuleiro);
+    posicionarNavios(tabuleiro);
+    exibirTabuleiro(tabuleiro);
 
     return 0;
 }
+
